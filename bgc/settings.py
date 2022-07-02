ba58ac781644e83097bd7d72d28adfbb3cab8877
edenbam <edenbam@gmail.com>
2022-07-02 15:02:56 -0300

    Configuração secret key heroku

--- bgc/settings.py
+++ bgc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5zqxk0lt*gbo@1dbe)8n8bm_l4ga8um$rau5=j4jow%_hg5az+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
