45934720896c62d2957a8cb021e5fe6f2c887620
edenbam <edenbam@gmail.com>
2022-07-03 00:49:49 -0300

    Alterado Procfile para Heroku

--- bgc/settings.py
+++ bgc/settings.py
@@ -139,7 +139,6 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # Caso o AWS_ACCESS_KEY_ID exista, as outras configurações são extraídas
 if AWS_ACCESS_KEY_ID:
-    print('Foi')
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
