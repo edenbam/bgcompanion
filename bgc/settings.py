31885d809c46b340df3e1cf220435d271b284cc8
edenbam <edenbam@gmail.com>
2022-07-04 21:06:26 -0300

    Inserido template

--- bgc/settings.py
+++ bgc/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-
 from functools import partial
 import os
 from pathlib import Path
@@ -129,6 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URAL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Storage configuration in S3 AWS
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
